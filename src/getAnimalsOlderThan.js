8bdbb9aad3bd1dde672d6b200729d92b13a01d6e
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-29 16:17:28 -0300

    Resolução requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  let animalsAge = false;
+  species.forEach((specie) => {
+    if (specie.name === animal) {
+      animalsAge = specie.residents.every((resident) => resident.age >= age);
+    }
+  });
+
+  return animalsAge;
 }
 
 module.exports = getAnimalsOlderThan;
