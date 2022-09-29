04fab8cbf3a21aadde6ac256de3c97e7aa3b80b9
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-29 15:56:12 -0300

    Resolução requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const speciesList = [];
+  ids.forEach((id) => speciesList.push(species.find((specie) => (specie.id === id))));
+
+  return speciesList;
 }
 
 module.exports = getSpeciesByIds;
