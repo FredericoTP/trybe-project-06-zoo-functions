7e398b6db903129e9195b086e720e72a1da345cf
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-29 17:45:59 -0300

    Resolução requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const employeeObject = employees.find((employee) => employee.id === id);
+  const { managers } = employeeObject;
+  if (managers.length === 0 || managers[0] === '9e7d4524-363c-416a-8759-8aa7e50c0992') {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const manages = employees.filter((employee) => employee.managers.includes(managerId));
+    return manages.map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
