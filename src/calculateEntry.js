e87f620ab11245aa6743962f711295b35cc748cf
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-29 19:40:04 -0300

    Resolução requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const { prices } = data;
 
+function countEntrants(entrants) {
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  const object = { child, adult, senior };
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Array.isArray(entrants) === false) {
+    return 0;
+  }
+
+  const total = countEntrants(entrants);
+  const { child, adult, senior } = total;
+  const sum = child * prices.child + adult * prices.adult + senior * prices.senior;
+
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// console.log(calculateEntry({}));
